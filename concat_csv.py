a9db571950006a82489b6d9148956db37a603d49
whitebear909 <insu.park80@gmail.com>
2019-10-07 16:59:57 +0900

    concat 모듈화

--- concat_csv.py
+++ concat_csv.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from tqdm import tqdm_gui
 
 TARGET_ALL = 0
 TARGET_CITY = 1
@@ -19,11 +20,14 @@ def concat_csv(path_dir):
     file_list = os.listdir(path_dir)  # path에 존재하는 파일 읽기
     file_list.sort()  # 파일 이름순서로 정렬
 
-    for index, file in enumerate(file_list):
+    for i in tqdm_gui(range(len(file_list)), desc='Merging progress'):
+        file = file_list[i]
         path = path_dir + '{}'.format(file)
         df_temp = pd.read_csv(path, sep=',', encoding="euc-kr")
-        if index == 0: df = df_temp.copy()
-        else : df = pd.concat([df, df_temp])
+        if i == 0:
+            df = df_temp.copy()
+        else :
+            df = pd.concat([df, df_temp], axis = 0, sort=False)
 
     return df
 
@@ -54,7 +58,7 @@ if __name__ == '__main__':
         make_file(to_path, file_name, source_path, TARGET_PORT)
     elif TARGET == TARGET_APP:
         # 항공 스케줄 공급
-        source_path = "D:/ML_Data/air/app/"
+        source_path = "D:/ML_Data/air/app/data/"
         file_name = 'air_schedule_app'
         df = make_file(to_path, file_name, source_path, TARGET_APP)
         # df = pd.read_json(to_path + file_name + "_total" + ".json", orient='table')
